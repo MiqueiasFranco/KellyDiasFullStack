3b89c0599f52deb84c5a503a1f00bc70f7f32010
MiqueiasFranco <miqueiassantos1552@gmail.com>
2025-03-16 17:43:54 -0300

    edicao de rotas

--- backend/src/router.js
+++ backend/src/router.js
@@ -4,9 +4,9 @@ const clientController = require('./controllers/clientController')
 const clientMiddlewares = require('./middlewares/clientMiddlewares')
 
 
-router.get('/clients', clientController.getALL)
-router.post('/clients', clientMiddlewares.validateFieldName, clientController.createClient)
-router.delete('/clients/:id', clientController.deleteClient)
-router.put('/clients/:id', clientMiddlewares.validateFieldService, clientMiddlewares.validateFieldName, clientController.updatedClient)
+router.get('/', clientController.getALL)
+router.post('/', clientMiddlewares.validateFieldName, clientController.createClient)
+router.delete('/:id', clientController.deleteClient)
+router.put('/:id', clientMiddlewares.validateFieldService, clientMiddlewares.validateFieldName, clientController.updatedClient)
 
 module.exports = router
