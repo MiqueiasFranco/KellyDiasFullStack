c50524f6af14f441e2e04f740461484427274c98
MiqueiasFranco <miqueiassantos1552@gmail.com>
2025-03-18 17:00:37 -0300

    alteracao link fetch

--- backend/src/modules/connection.js
+++ backend/src/modules/connection.js
@@ -6,7 +6,6 @@ const connection = mysql.createPool({
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DB,
-    port: process.env.PORT
 })
 
 module.exports = connection
